5266e2cb22c86cc5cb0ee9ebe1967f3c59c0434b
teomw <teomw02@gmail.com>
2026-05-17 19:24:00 +0800

    Updated comments for dockerfile

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-# Using the database service name from docker-compose
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@db:5432/maistorage")
 # Use localhost when running on Windows, use 'db' when running in Docker
 # DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/maistorage")
@@ -13,8 +12,8 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
-    db = SessionLocal()
+    db = SessionLocal() # Borrow connection fromt the pool
     try:
-        yield db
+        yield db  # Give connection to FastAPI
     finally:
-        db.close()
+        db.close()  # Return connection to the pool
